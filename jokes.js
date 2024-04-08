bce760cf9049bb5d15fa4fc50b43d783e4d2af98
LAKSHMI-SARVAJNA <114597600+LAKSHMI-SARVAJNA@users.noreply.github.com>
2024-04-08 15:38:35 +0530

    Add files via upload

---
+++ jokes.js
@@ -0,0 +1,332 @@
+let myjokes = [ {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Programming is 10% science, 20% ingenuity, and 70% getting the ingenuity to work with the science.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 37,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Today I learned that changing random stuff until your program works is \"hacky\" and a \"bad coding practice\" but if you do it fast enough it's \"Machine Learning\" and pays 4x your current salary.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 33,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Dark",
+    "type": "single",
+    "joke": "I don't know where I stand on abortion. I like killing babies, but I don't like giving women a choice.",
+    "flags": {
+        "nsfw": true,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": true,
+        "explicit": true
+    },
+    "id": 228,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "A SQL statement walks into a bar and sees two tables.\nIt approaches, and asks \"may I join you?\"",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 5,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "If you're here for the yodeling lesson, please form an orderly orderly orderly queue.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 139,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Pun",
+    "type": "single",
+    "joke": "Oysters hate to give away their pearls because they are shellfish.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 90,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Dark",
+    "type": "single",
+    "joke": "My grandfather says I'm too reliant on technology.\nI called him a hypocrite and unplugged his life support.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 178,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "My parents raised me as an only child, which really annoyed my younger brother.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 235,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "Stop being homophobic and rude to the LGBTQ+ community. You should be thanking them for saving us plenty of room in heaven.",
+    "flags": {
+        "nsfw": false,
+        "religious": true,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 114,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Misc",
+    "type": "single",
+    "joke": "Never date a baker. They're too kneady.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "safe": true,
+    "id": 271,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Java is like Alzheimer's, it starts off slow, but eventually, your memory is gone.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 27,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Debugging is like being the detective in a crime movie where you're also the murderer at the same time.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 42,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Saying that Java is nice because it works on every OS is like saying that anal sex is nice because it works on every gender.",
+    "flags": {
+        "nsfw": true,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": true
+    },
+    "id": 30,
+    "safe": false,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "Eight bytes walk into a bar.\nThe bartender asks, \"Can I get you anything?\"\n\"Yeah,\" reply the bytes.\n\"Make us a double.\"",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 34,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "\"We messed up the keming again guys.\"",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 20,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "// This line doesn't actually do anything, but the code stops working when I delete it.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 12,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "ASCII silly question, get a silly ANSI.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 25,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "\"Knock, knock.\"\n\"Who's there?\"\n\n[very long pause]\n\n\"Java.\"",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 36,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "The generation of random numbers is too important to be left to chance.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 39,
+    "safe": true,
+    "lang": "en"
+},
+{
+    "category": "Programming",
+    "type": "single",
+    "joke": "A programmer puts two glasses on his bedside table before going to sleep.\nA full one, in case he gets thirsty, and an empty one, in case he doesn't.",
+    "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+    },
+    "id": 3,
+    "safe": true,
+    "lang": "en"
+}
+]
+
+let index = Math.floor(Math.random() * (myjokes.length-1))
+card.innerHTML = myjokes[index].joke;
+
+ function click_button() {
+    let index = Math.floor(Math.random() * (myjokes.length-1))
+
+card.innerHTML = myjokes[index].joke;
+
+ }
+
